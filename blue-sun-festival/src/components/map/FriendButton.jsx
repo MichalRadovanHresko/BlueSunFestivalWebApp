9801e342b900437c241e52511d6a6b8a2df8f770
Misko <m.hresko.dev@gmail.com>
2026-05-20 11:14:20 +0200

    Fixed stylign for friend button

--- blue-sun-festival/src/components/map/FriendButton.jsx
+++ blue-sun-festival/src/components/map/FriendButton.jsx
@@ -3,30 +3,28 @@ import QueueIcon from "../icons/QueueIcon";
 import LocationIcon from "../icons/LocationIcon";
 import Discover from "../../assets/icons/discoverIcon.svg";
 const FriendButton = () => {
-  const styling =
-    "absolute bottom-[7%] bg-[white] shadow-lg shadow-[0_4px_10px_rgba(0,0,0,0.3)] rounded-[50%] h-[30px] w-[30px] p-[15px] z-10 m-[2px]";
+  const sideBtn =
+    "bg-[white] shadow-lg shadow-[0_4px_10px_rgba(0,0,0,0.3)] rounded-[50%] h-[30px] w-[30px] p-[15px] m-[2px] z-10";
 
   return (
-    <>
+    <div className="absolute bottom-[7%] w-full flex justify-around items-end z-10">
       <NavLink to="/">
-        <div className={`${styling} left-[4%]`}>
+        <div className={sideBtn}>
           <LocationIcon />
         </div>
       </NavLink>
-      <div
-        className={`absolute bottom-[7%] left-[35%] bg-[white] flex flex-col justify-center items-center rounded-[30px] w-[130px] h-[110px]`}
-      >
+      <div className="bg-[white] flex flex-col justify-center items-center rounded-[30px] w-[130px] h-[110px]">
         <div className="rounded-[50%] bg-[#0199D7] p-[15px]">
           <img src={Discover} alt="Discover" height={20} width={25} />
         </div>
         <p className="m-[0px] text-[#22428F] text-[16px]">Discover</p>
       </div>
       <NavLink to="/crowd/density">
-        <div className={`${styling} right-[4%]`}>
+        <div className={sideBtn}>
           <QueueIcon />
         </div>
       </NavLink>
-    </>
+    </div>
   );
 };
 
